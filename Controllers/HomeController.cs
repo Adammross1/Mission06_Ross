e46c11146fdf504e67cddd051a5bd5dfc98a4be2
Adammross1 <adamross202@gmail.com>
2024-02-12 20:39:10 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission06_Ross.Models;
+using System.Diagnostics;
+
+namespace Mission06_Ross.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
